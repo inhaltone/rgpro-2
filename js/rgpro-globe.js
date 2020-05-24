8f86bf36babb0cc822d98e1f541fe4778b283759
inhalt <56723288+inhaltone@users.noreply.github.com>
2020-05-24 20:42:05 +0300

    module upadate

--- js/rgpro-globe.js
+++ js/rgpro-globe.js
@@ -1,7 +1,6 @@
-/*import * as THREE from '../build/three.module.js';
+import * as THREE from '../build/three.module.js';
 import {OrbitControls} from '../build/OrbitControls.js';
 import {GUI} from '../build/dat.gui.module.js';
-*/
 var Shaders = {
     'earth' : {
       uniforms: {
